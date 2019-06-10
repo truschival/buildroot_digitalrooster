f4e7afaa790cd6e01bfd1c4f3b4de25dc236206d
Thomas Ruschival <thomas@ruschival.de>
2019-06-10 16:32:03 +0200

    Fix wpa_supplicant startup script

--- board/common/rootfs_overlay/etc/init.d/S39wpa_supplicant.sh
+++ board/common/rootfs_overlay/etc/init.d/S39wpa_supplicant.sh
@@ -6,27 +6,25 @@ DAEMON=/usr/sbin/wpa_supplicant
 DAEMON_ARGS="-i wlan0 -Dwext -c/etc/wpa_supplicant.conf "
 PIDFILE=/var/run/wpa_supplicant.pid
 
-sleep 1
-
-case "\$1" in
-  start)
-		printf "Starting WPA Supplicant:"
-		start-stop-daemon -p \$PIDFILE -x \$DAEMON -v -m -b -S -- \$DAEMON_ARGS
-		[ \$? = 0 ] && echo "OK" || echo "FAIL"
-		sleep 2
-        ;;
-  stop)
-        printf "Stopping WPA Supplicant: "
-		start-stop-daemon -p \$PIDFILE -K
-        [ \$? = 0 ] && echo "OK" || echo "FAIL"
-        ;;
-  restart|reload)
-        "\$0" stop
-        "\$0" start
-        ;;
-  *)
-        echo "Usage: \$0 {start|stop|restart}"
-        exit 1
+case "$1" in
+    start)
+	printf "Starting WPA Supplicant:"
+	start-stop-daemon -p $PIDFILE -x $DAEMON -v -m -b -S -- $DAEMON_ARGS
+	[ $? = 0 ] && echo "OK" || echo "FAIL"
+	sleep 2
+	;;
+    stop)
+	printf "Stopping WPA Supplicant: "
+	start-stop-daemon -p $PIDFILE -K
+	[ $? = 0 ] && echo "OK" || echo "FAIL"
+	;;
+    restart|reload)
+	"$0" stop
+	"$0" start
+	;;
+    *)
+	echo "Usage: $0 {start|stop|restart}"
+	exit 1
 esac
 
-exit \$? 
+exit $?
