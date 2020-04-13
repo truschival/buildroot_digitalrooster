c53435c3fbac8917c8a3a1e001b4161b497be541
Thomas Ruschival <thomas@ruschival.de>
2020-04-13 19:13:14 +0200

    Update Pistache + enable SSL
    Signed-off-by: Thomas Ruschival <thomas@ruschival.de>

--- package/pistache/pistache.mk
+++ package/pistache/pistache.mk
@@ -4,15 +4,16 @@
 #
 ################################################################################
 
-PISTACHE_VERSION = 394b17c01f928bb
-PISTACHE_SITE = https://github.com/oktal/pistache.git
-PISTACHE_SITE_METHOD = git
+PISTACHE_VERSION = 73f248acd6db4c53
+PISTACHE_SOURCE = $(PISTACHE_VERSION).tar.gz
+PISTACHE_SITE = https://github.com/oktal/pistache/archive
+PISTACHE_SITE_METHOD = wget
 
 PISTACHE_INSTALL_STAGING = YES
 PISTACHE_INSTALL_TARGET = YES
 PISTACHE_LICENSE = Apache-2.0
 PISTACHE_LICENSE_FILE = LICENSE
 
-PISTACHE_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
+PISTACHE_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release -DPISTACHE_USE_SSL=On
 
 $(eval $(cmake-package))
