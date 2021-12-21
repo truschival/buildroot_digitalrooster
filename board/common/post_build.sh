c8aeb72aa7a179ee3dc1c2935ba777ea2b62b500
Thomas Ruschival <thomas@ruschival.de>
2021-12-21 15:08:57 +0100

    Add local ssh public key to target
    Signed-off-by: Thomas Ruschival <thomas@ruschival.de>

--- board/common/post_build.sh
+++ board/common/post_build.sh
@@ -5,24 +5,41 @@ function update_wpa_config() {
     local WPA_SUPPLICANT_CFG_PATH=${TARGET_DIR}/etc/wpa_supplicant.conf
     local WIFI_NET_GUARD="# LOCAL_WIFI_NET_CFG updated by common/post_build.sh"
     local NETCONF=""
+    echo ">Updating WPA Supplicant config LOCAL_WIFI_NET_CFG=${LOCAL_WIFI_NET_CFG}"
 
     if [ -e ${WPA_SUPPLICANT_CFG_PATH} ] && [ ! -z "${LOCAL_WIFI_NET_CFG}" ];
     then
 	# Check if netconf file exists
 	if [ ! -e ${LOCAL_WIFI_NET_CFG} ] ;
 	then
-	    echo "${LOCAL_WIFI_NET_CFG} not found"
+	    echo " ${LOCAL_WIFI_NET_CFG} not found"
 	    return
 	fi
-
 	grep -q "${WIFI_NET_GUARD}" ${WPA_SUPPLICANT_CFG_PATH}
 	if [ $? -eq 1 ];
 	then
 	    echo ${WIFI_NET_GUARD} >> ${WPA_SUPPLICANT_CFG_PATH}
 	    cat < ${LOCAL_WIFI_NET_CFG} >> ${WPA_SUPPLICANT_CFG_PATH}
+	    echo " OK"
 	else
-	    echo "post_build.sh already modified ${WPA_SUPPLICANT_CFG_PATH} - not changing!"
+	    echo " skipped (post_build.sh already modified ${WPA_SUPPLICANT_CFG_PATH})"
 	fi
+    fi
+}
+
+# Add ssh pubkey for root user
+function update_root_ssh() {
+    local SSH_AUTHORIZED_KEYS_PATH=${TARGET_DIR}/root/.ssh/authorized_keys
+    echo ">Updating root ssh key $SSH_PUBKEY_FILE"
+    # Check if a local pubkey is given and if it exists
+    if [ ! -z "$LOCAL_SSH_PUBKEY" ] && [ -e $LOCAL_SSH_PUBKEY ] ;
+    then
+	mkdir -p  $(dirname $SSH_AUTHORIZED_KEYS_PATH)
+	cat < $LOCAL_SSH_PUBKEY > $SSH_AUTHORIZED_KEYS_PATH
+	chmod 600 $SSH_AUTHORIZED_KEYS_PATH
+	echo " OK"
+    else
+	echo " skipped"
     fi
 }
 
@@ -34,6 +51,7 @@ function update_fstab() {
     local FSTAB_BOOT_ENTRY="/dev/mmcblk0p1	/boot	vfat    defaults        0       0"
     local FSTAB_PERSISTENT_ENTRY="/dev/mmcblk0p4	/persistent	ext4    defaults        0       0"
 
+    echo ">Updating /etc/fstab"
     mkdir -p "${TARGET_DIR}/boot"
 
     grep -q "${FSTAB_PERSISTENT_GUARD}" ${FSTAB_PATH}
@@ -43,24 +61,23 @@ function update_fstab() {
 	echo ${FSTAB_BOOT_ENTRY} >> ${FSTAB_PATH}
 	echo ${FSTAB_PERSISTENT_ENTRY} >> ${FSTAB_PATH}
     else
-	echo "post_build.sh already modified ${FSTAB_PATH} - not changing!"
+	echo " skipped (post_build.sh already modified ${FSTAB_PATH})"
     fi
 }
 
 function set_version_info(){
     EXTERNAL_DIR="$(dirname $0)"
     GIT_DESC=$(git -C $EXTERNAL_DIR describe --dirty)
+    echo ">Setting Version_info $GIT_DESC"
     local ts=$(date +%Y-%m-%d_%H:%M:%S)
     echo "REVISION=$GIT_DESC" >  ${TARGET_DIR}/etc/digitalrooster_build_info
     echo "BUILD_DATE=$ts"     >> ${TARGET_DIR}/etc/digitalrooster_build_info
 }
 
-echo "> Updating WPA Supplicant config LOCAL_WIFI_NET_CFG=${LOCAL_WIFI_NET_CFG}"
 update_wpa_config
-echo "> Updating /etc/fstab"
 update_fstab
-echo "> Setting Version_info"
 set_version_info
+update_root_ssh
 
 ##
 # If a local environment variable for the public key certificate exists copy
